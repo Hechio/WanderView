19d66fabc0e25828483bbe1b140c7ab26e2726a7
Stephen Henry <47601553+Hechio@users.noreply.github.com>
2023-08-15 21:46:10 +0300

    Initial Commit

---
+++ WanderView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WanderView
+//
+//  Created by Steve Hechio on 15/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
